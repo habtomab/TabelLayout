ee049679b6bfd37c8f3bcfe895ce20713de43bed
emito <aigemito.2006@gmail.com>
2019-11-29 00:30:30 -0600

    Initial commit

---
+++ app/src/main/java/com/emito/tabellayout/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.emito.tabellayout
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Space
+import android.widget.TableRow
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickAdd(view: View) {
+        // Create a new table row.
+        val tableRow = TableRow(getApplicationContext())
+        // Set new table row layout parameters.
+        val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+        layoutParams.setMargins(0,4,4,4)
+        tableRow.setLayoutParams(layoutParams)
+        //Create the two columns as a TextView Controls
+        if (etAndroidVersion.text.toString().isNotEmpty() && etAndroidCodeName.text.toString().isNotEmpty()){
+            val tv1=TextView(getApplicationContext())
+            tv1.setText(etAndroidVersion.text.toString())
+            tv1.layoutParams=layoutParams
+
+            val tv2=TextView(getApplicationContext())
+            tv2.setText(etAndroidCodeName.text.toString())
+            tv2.layoutParams=layoutParams
+            // add values into row by calling addView()
+            tv1.setBackgroundColor(getResources().getColor(R.color.myColor))
+            tv2.setBackgroundColor(getResources().getColor(R.color.myColor))
+
+            tableRow.addView(tv1)
+            tableRow.addView(tv2)
+            //tableRow.addView(sp)
+        }
+
+
+
+        // Finally add the created row row into layout
+        tableLayout.addView(tableRow)
+    }
+}
